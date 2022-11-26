210e72046aa7141957ea4d47c02a46252de3cf83
sinhayash9546 <sinhayash9546@gmail.com>
2022-11-26 13:27:59 +0530

    Project Files v1.0

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const multer  = require('multer')
+
+//importing my newly created module named mergePdfs
+const {mergePdfs}  = require('./merge')
+
+const upload = multer({ dest: 'uploads/' })
+
+
+app.use('/static', express.static('public'))
+
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"))
+})
+
+
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+  console.log(req.files);
+  let data = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+  res.redirect(`http://localhost:3000/static/${data}.pdf`);
+  // res.send({data: req.files})
+  // req.files is array of `photos` filesf
+  // req.body will contain the text fields, if there were any
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
